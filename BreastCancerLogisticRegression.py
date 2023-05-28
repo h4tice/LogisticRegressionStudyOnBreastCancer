b10b6e047accbd4b49f7dbef59ba3969c2ac06f7
Hatice Karaca <haticekrc772@gmail.com>
2023-05-28 15:35:24 +0300

    Logistic Regression Study on Breast Cancer

---
+++ BreastCancerLogisticRegression.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+#Verilerin Import Edilmesi
+dataset = pd.read_csv("breast_cancer.csv")
+
+X = dataset.iloc[:,1:-1].values
+y = dataset.iloc[:,-1].values
+
+#Train ve Test Setleri
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, 
+                                                    test_size=0.2, 
+                                                    random_state=0)
+
+#Logistic Regression Modelinin Kurulması
+from sklearn.linear_model import LogisticRegression
+classifier = LogisticRegression(random_state=0)
+
+
+#Modelin Train Set Üzerinde Eğitilmesi
+classifier.fit(X_train, y_train)
+
+
+#Modelin Test Edilmesi
+y_pred = classifier.predict(X_test)
+y_pred_prob = classifier.predict_proba(X_test)
+
+
+#Confusion Matrix ve AccuracyScore
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+
+cm = confusion_matrix(y_test,y_pred)
+AccuracyScore = accuracy_score(y_test,y_pred)
+
+
+#k-Fold Cross Validation Yöntemiyle Modelin Preformansının Ölçülmesi
+from sklearn.model_selection import cross_val_score
+accuricies = cross_val_score(estimator = classifier, X = X, y = y, cv = 10)
+AccuriciesMean = accuricies.mean()*100
+StandartDeviation = accuricies.std()*100
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
